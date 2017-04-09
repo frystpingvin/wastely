51a5adab74df75c256294633b0f76312666055ff
Samuel Lie <samuel.lie1@gmail.com>
2017-04-09 17:09:41 +0200

    Config, styling and copy update

--- config.rb
+++ config.rb
@@ -42,8 +42,22 @@ activate :directory_indexes
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
+# Use relative URLs
+activate :relative_assets
+set :relative_links, true
+
 # configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :minify_html
 # end
+
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+  deploy.build_before = true
+end
